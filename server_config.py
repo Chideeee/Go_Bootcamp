e130e55bafade52ee6ee9dd33746cf4083071eb3
chidiebere unegbu <chidiebereunegbu@yahoo.com>
2024-01-23 12:21:05 +0100

    server config script added

---
+++ server_config.py
@@ -0,0 +1,10 @@
+import getpass
+
+"""
+The fabric module in python is used for connecting to
+and configuring servers remotely.
+To use it, it is required that the required packages from
+the fabric module be imported and used.
+"""
+
+from fabric import Connection, Config
